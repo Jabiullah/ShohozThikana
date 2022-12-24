2d186277e3619658a03bb03d3d21b2252b59b181
NsNipa <nipasaha183@gmail.com>
2022-12-24 15:26:05 +0600

    map  2nd commit ...

---
+++ app/src/main/java/com/example/filetranslation/MapDemo.java
@@ -0,0 +1,14 @@
+package com.example.filetranslation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_demo);
+    }
+}
