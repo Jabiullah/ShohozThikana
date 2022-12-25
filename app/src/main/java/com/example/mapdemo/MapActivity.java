1d3c897ae33ea5a8ac12eeb1dff03da70df27837
NsNipa <nipasaha183@gmail.com>
2022-12-25 13:09:14 +0600

    New feature

---
+++ app/src/main/java/com/example/mapdemo/MapActivity.java
@@ -0,0 +1,43 @@
+package com.example.mapdemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap gMap;
+    FrameLayout map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        map = findViewById(R.id.map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        this.gMap = googleMap;
+
+        LatLng mapBangladesh = new LatLng(23.6850, 90.3563);
+        this.gMap.addMarker(new MarkerOptions().position(mapBangladesh).title("Maker is Bangladesh"));
+        this.gMap.moveCamera(CameraUpdateFactory.newLatLng(mapBangladesh));
+
+    }
+}
